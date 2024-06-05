77965ba34747205d0217e6059b81963f366e079d
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-06-05 17:44:20 +0900

    외부 API 불러오기  강사님 코드

--- DAta.js
+++ DAta.js
@@ -1,21 +1,11 @@
 const a = `
-https://apis.data.go.kr/6300000/eventDataService/
-eventDataListJson
-?
-
-serviceKey=7B0UdKJ9T4IeLL7vj40OSMderbRN3mFXhyra69Q2rlGmiPq9J1xEQcKxka6i2x8ywX9eDEW8qor5Keh8a0trgA%3D%3D&`;
-
+https://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=7B0UdKJ9T4IeLL7vj40OSMderbRN3mFXhyra69Q2rlGmiPq9J1xEQcKxka6i2x8ywX9eDEW8qor5Keh8a0trgA%3D%3D& `;
 const b = "endpoint 주소 " + "주소값";
 const endpoint = "	http://apis.data.go.kr/6300000/eventDataService";
 
 const KeyName = `7B0UdKJ9T4IeLL7vj40OSMderbRN3mFXhyra69Q2rlGmiPq9J1xEQcKxka6i2x8ywX9eDEW8qor5Keh8a0trgA%3D%3D`;
 
 const exaple = `
-http://apis.data.go.kr/6300000/eventDataService/
-eventDataItemJson
-?
-serviceKey= ${KeyName}
-&
-eventSeq=10423
+http://apis.data.go.kr/6300000/eventDataService/eventDataItemJson?serviceKey= ${KeyName}&eventSeq=10423
 `;
 console.log(exaple);
